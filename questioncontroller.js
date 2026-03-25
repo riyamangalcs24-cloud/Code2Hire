54679a8070eb18bef4aeb23af3c57d0ddd17ad4f
Shagun Gupta <shagung072@gmail.com>
2026-03-25 08:56:37 +0530

    Add files via upload

---
+++ questioncontroller.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const Question = require('./question');
+
+router.get('/', async (req, res) => {
+  try {
+    const questions = await Question.find({});
+    res.json(questions);
+  } catch (error) {
+    res.status(500).json({ message: 'Server error while fetching questions' });
+  }
+});
+
+module.exports = router;
